813047e796b6817d388cc4b192e283083156eef7
John Maeda <johnmaeda@users.noreply.github.com>
2019-05-11 15:49:45 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'CX Report',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'CX is Computation Experience and Customer Experience and Consumer Experience',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
